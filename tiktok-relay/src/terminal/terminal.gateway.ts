91ec5db81694e018952c4d4418b967bb83e1d282
wxyy <wahyu1824joingd@gmail.com>
2026-04-25 12:04:46 +0700

    refactor: streamline TerminalGateway by removing unused methods and simplifying message handling

--- tiktok-relay/src/terminal/terminal.gateway.ts
+++ tiktok-relay/src/terminal/terminal.gateway.ts
@@ -2,18 +2,12 @@ import {
   WebSocketGateway,
   WebSocketServer,
   OnGatewayConnection,
-  OnGatewayDisconnect,
-  SubscribeMessage,
-  MessageBody,
-  ConnectedSocket,
 } from '@nestjs/websockets';
 
 import { Server, WebSocket } from 'ws';
 
 @WebSocketGateway()
-export class TerminalGateway
-  implements OnGatewayConnection, OnGatewayDisconnect
-{
+export class TerminalGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
@@ -22,21 +16,12 @@ export class TerminalGateway
 
   handleConnection(client: WebSocket) {
     console.log('Client connected');
-  }
 
-  handleDisconnect(client: WebSocket) {
-    console.log('Client disconnected');
+    client.on('message', (msg: string) => {
+      const data = JSON.parse(msg.toString());
 
-    if (this.windowsClient === client) this.windowsClient = null;
-    if (this.browserClient === client) this.browserClient = null;
-  }
+      console.log('Message:', data);
 
-  @SubscribeMessage('message')
-  handleMessage(
-    @MessageBody() data: any,
-    @ConnectedSocket() client: WebSocket,
-  ) {
-    try {
       if (data.role === 'windows') {
         this.windowsClient = client;
         console.log('Windows connected');
@@ -47,22 +32,21 @@ export class TerminalGateway
         console.log('Browser connected');
       }
 
-      if (data.type === 'output' && this.browserClient) {
-        this.browserClient.send(JSON.stringify(data));
+      if (data.type === 'start' && this.windowsClient) {
+        this.windowsClient.send(JSON.stringify({ type: 'start' }));
+      }
+
+      if (data.type === 'stop' && this.windowsClient) {
+        this.windowsClient.send(JSON.stringify({ type: 'stop' }));
       }
 
       if (data.type === 'input' && this.windowsClient) {
         this.windowsClient.send(JSON.stringify(data));
       }
 
-      if (
-        (data.type === 'start' || data.type === 'stop') &&
-        this.windowsClient
-      ) {
-        this.windowsClient.send(JSON.stringify(data));
+      if (data.type === 'output' && this.browserClient) {
+        this.browserClient.send(JSON.stringify(data));
       }
-    } catch (e) {
-      console.log('Message error');
-    }
+    });
   }
 }
